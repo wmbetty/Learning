d35d9f57cec2a875231f211758e7cf3aed3628f5
wmbetty <18819343820@163.com>
2018-04-17 12:44:11 +0800

    找去处详情页完善，对接点赞接口

--- wii-line-manager/pages/previewDetail/previewDetail.js
+++ wii-line-manager/pages/previewDetail/previewDetail.js
@@ -13,15 +13,28 @@ Page({
   data: {
     spotId: '', //地点id
     spotDetail: {},
-    intros: ''
+    intros: '',
+    winHeight: '',
+    sid: '',
+    detailData: {},
+    coLove: 0, //点赞数
+    bklove: false //点赞
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let that = this
+    wxJs.getSystemInfo((res) => {
+      // 可使用窗口宽度、高度
+      let windowHeight = res.windowHeight
+      that.setData({
+        // second部分高度 = 利用窗口可使用高度 - first部分高度（这里的高度单位为px，所有利用比例将300rpx转换为px）
+        winHeight: windowHeight
+      })
+    })
     let item = JSON.parse(options.item)
-    console.log(item, 'item')
     let id = (item.id).substring(5)
     let sid = wx.getStorageSync('sid')
     if (sid === '') {
@@ -36,15 +49,12 @@ Page({
         'platform':platform,
         'ver':ver
       }
-      wxJs.postRequest(url, postData, (res) => {
-        let details = res.data.result.Baike
-        let that = this
-        let intros = details.content.replace(/(^\s*)|(\s*$)/g, "")
-        that.setData({
-          spotDetail: details,
-          intros: intros
-        })
+      that.setData({
+        sid: sid,
+        detailUrl: url,
+        detailData: postData
       })
+      that.getDetails(url, postData)
     }
   },
 
@@ -95,5 +105,61 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  getDetails (url, postData) {
+    let that = this
+    wxJs.postRequest(url, postData, (res) => {
+      let details = res.data.result.Baike
+      let intros = details.content.replace(/(^\s*)|(\s*$)/g, "")
+      let coLove = details.coLove
+      that.setData({
+        spotDetail: details,
+        intros: intros,
+        coLove: coLove
+      })
+    })
+  },
+
+  // 点赞
+  bkLove() {
+    let that = this
+    let sourceId = that.data.spotDetail.id
+    let targetId = that.data.spotDetail.myId
+    let url = app.globalData.url + '/bkLove/bkLove?sid=' + that.data.sid
+    let bklove = that.data.bklove
+    let postData = {
+      'sourceType': 'Baike',
+      'sourceId': sourceId * 1,
+      'realType': '',
+      'realId': 0,
+      'targetId': targetId * 1,
+      'app': appValue,
+      'platform': platform,
+      'ver': ver
+    }
+    if (bklove) {
+
+    } else{
+      wxJs.postRequest(url, postData, (res) => {
+        let data = res.data.result.Delete
+        let url = that.data.detailUrl
+        if (data) {
+          that.setData({
+            coLove: that.data.coLove * 1 + 1,
+            bklove: !bklove
+          })
+        }
+        // let details = res.data.result.Baike
+        // let intros = details.content.replace(/(^\s*)|(\s*$)/g, "")
+        // that.setData({
+        //   spotDetail: details,
+        //   intros: intros,
+        //   sid: sid,
+        //   lastUrl: url
+        // })
+      })
+    }
   }
+
 })
