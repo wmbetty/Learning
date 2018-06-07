f3ce143f9b735ef6bc4d221bbdcf1fa007ee3ab0
王梅 <wangmei@MacBook-Air.local>
2018-06-07 11:09:18 +0800

    发布页UI基本完成

--- choice/pages/index/index.js
+++ choice/pages/index/index.js
@@ -7,7 +7,11 @@ Page({
     showLeft: false,
     showRight: false,
     leftHolder: '点击输入左选项',
-    rightHolder: '点击输入右选项'
+    rightHolder: '点击输入右选项',
+    isPublish: false,
+    leftText: '',
+    rightText: '',
+    titleText: ''
   },
   onLoad: function(option) {
     tabBar.tabbar("tabBar", 2, this);//0表示第一个tabbar
@@ -26,13 +30,14 @@ Page({
   contFocus (e) {
     let that = this;
     let direct = e.target.dataset.direct;
-    if (direct === 'left') {
+    if (direct === 'left' && that.data.leftHolder === '点击输入左选项') {
       that.setData({
         showLeft: true,
         leftHolder: ''
       })
-    } else {
-      this.setData({
+    }
+    if (direct === 'right' && that.data.rightHolder === '点击输入右选项') {
+      that.setData({
         showRight: true,
         rightHolder: ''
       })
@@ -41,9 +46,57 @@ Page({
   titlePut (e) {
     let that = this;
     let val = e.detail.value;
+    let direct = e.target.dataset.direct;
     let chineseReg = /[\u4E00-\u9FA5]/g;
+    // 标题字数限制
     if (val.trim().length > 30) {
       console.log('超出了')
     }
+    if (direct === 'title') {
+      that.setData({
+        titleText: val
+      })
+    }
+    if (direct === 'left') {
+      that.setData({
+        leftText: val
+      })
+    }
+    if (direct === 'right') {
+      that.setData({
+        rightText: val
+      })
+    }
+    // 发布按钮样式
+    if (that.data.titleText !== '' && that.data.leftText !== '' && that.data.rightText !== '') {
+      that.setData({
+        isPublish: true
+      })
+    } else {
+      that.setData({
+        isPublish: false
+      })
+    }
+  },
+  // 点击发布
+  goPublish () {
+    let that = this;
+    if (that.data.titleText === '' && that.data.leftText === '' && that.data.rightText === '') {
+      let wxShowToast = Api.wxShowToast('请填写基本内容', 'none', 2000);
+      return false;
+    }
+    if (that.data.titleText === '') {
+      let wxShowToast = Api.wxShowToast('请填写标题', 'none', 2000);
+      return false;
+    }
+    if (that.data.leftText === '') {
+      let wxShowToast = Api.wxShowToast('请填写左选项', 'none', 2000);
+      return false;
+    }
+    if (that.data.rightText === '') {
+      let wxShowToast = Api.wxShowToast('请填写右选项', 'none', 2000);
+      return false;
+    }
+    console.log(11)
   }
 });
