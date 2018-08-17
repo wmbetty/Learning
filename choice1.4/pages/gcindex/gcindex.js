31c1571d6dabc4b78af31221916844286b943c01
王梅 <wangmei@MacBook-Air.local>
2018-08-17 16:41:57 +0800

    选象1.4，增加首页，底部tab修改

---
+++ choice1.4/pages/gcindex/gcindex.js
@@ -0,0 +1,43 @@
+// pages/gcindex/gcindex.js
+
+const tabBar = require('../../components/tabBar/tabBar.js');
+
+
+Page({
+  data: {
+  
+  },
+
+  onLoad: function (options) {
+    let that = this;
+    tabBar.tabbar("tabBar", 0, that);
+  },
+
+  onReady: function () {
+  
+  },
+
+  onShow: function () {
+  
+  },
+
+  onHide: function () {
+  
+  },
+
+  onUnload: function () {
+  
+  },
+
+  onPullDownRefresh: function () {
+  
+  },
+
+  onReachBottom: function () {
+  
+  },
+
+  onShareAppMessage: function () {
+  
+  }
+})
